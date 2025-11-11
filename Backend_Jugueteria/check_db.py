43e811d2ae105af2c95de399382a2159587d4c9b
.gitValentina <you@example.com>
2025-11-11 14:03:08 -0500

    Corecciones v2

--- Backend_Jugueteria/check_db.py
+++ Backend_Jugueteria/check_db.py
@@ -9,13 +9,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_django.settings')
 django.setup()
 
 def check_database():
-    print("🔍 Verificando conexión con la base de datos MySQL...\n")
+    print("✉︎ Verificando conexión con la base de datos MySQL...\n")
     db_conn = connections['default']
     try:
         db_conn.cursor()
-        print("✅ Conexión establecida correctamente con MySQL.")
+        print("☑ Conexión establecida correctamente con MySQL.")
     except OperationalError as e:
-        print("❌ Error al conectar con MySQL.")
+        print(" Error al conectar con MySQL.")
         print(f"Detalles del error:\n{e}")
 
 if __name__ == "__main__":
