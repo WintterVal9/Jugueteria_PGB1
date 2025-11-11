322b7f61ee6c5a9ad0157aa6fb25189e0b41d34d
.gitValentina <you@example.com>
2025-11-11 10:12:06 -0500

    Adicion de check .py para verificar conexion de base de datos

---
+++ Backend_Jugueteria/check_db.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+import django
+from django.db import connections
+from django.db.utils import OperationalError
+
+# Configurar el entorno de Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_django.settings')
+django.setup()
+
+def check_database():
+    print("🔍 Verificando conexión con la base de datos MySQL...\n")
+    db_conn = connections['default']
+    try:
+        db_conn.cursor()
+        print("✅ Conexión establecida correctamente con MySQL.")
+    except OperationalError as e:
+        print("❌ Error al conectar con MySQL.")
+        print(f"Detalles del error:\n{e}")
+
+if __name__ == "__main__":
+    check_database()
