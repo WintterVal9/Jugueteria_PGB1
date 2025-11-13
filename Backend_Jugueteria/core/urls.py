e63e7ab2442662892a9c965687e4d6c4898ad1ce
.gitValentina <you@example.com>
2025-11-13 14:53:15 -0500

    Urls correcion en core

--- Backend_Jugueteria/core/urls.py
+++ Backend_Jugueteria/core/urls.py
@@ -2,15 +2,16 @@ from django.urls import path
 from . import views, views_productos
 
 urlpatterns = [
-    # 🔹 API
-    path('api/productos/', views.api_productos, name='api_productos'),
+    path('', views.index, name='index'),  # 👈 Esta es la ruta del index general
     path('verificar-conexion/', views.verificar_conexion, name='verificar_conexion'),
-
-    # 🔹 Usuario general
     path('productos/', views.lista_productos, name='lista_productos'),
 
-    # 🔹 Administración
+    # Administración
     path('admin/productos/', views_productos.lista_productos_admin, name='admin_productos'),
     path('admin/productos/nuevo/', views_productos.crear_producto, name='admin_crear_producto'),
+
+    # API
+    path('api/productos/', views.api_productos, name='api_productos'),
 ]
+
 
